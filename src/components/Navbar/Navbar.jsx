cf3dca76f72f43fa94c2205af2fb436d907bb05f
drakata27 <theall3xander@gmail.com>
2024-08-23 16:06:51 +0100

    Separated skills from experience

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,8 +23,9 @@ const Navbar = () => {
             >
                 <li><a href="#about">About</a></li>
                 <li><a href="#experience">Experience</a></li>
-                <li><a href="#about">Projects</a></li>
-                <li><a href="#about">Contact</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
         </div>
     </nav>
