24173f09a4e6a390b88b526d10e5588e37672318
drakata27 <theall3xander@gmail.com>
2024-08-26 10:46:40 +0100

    Project is completed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,6 @@ const Hero = () => {
     <div className={styles.content}>
         <h1 className={styles.title}>Hi, I am Aleks</h1>
         <p className={styles.description}>I am a Computer Science at the University of West London, currently working at NBCUniversal as a Media Technology Intern</p>
-        {/* <a href="mailto:aleks.draka02@gmail.com" className={styles.contactBtn} >Contact Me</a> */}
         <a href="#contact" className={styles.contactBtn}>Contact Me</a>
     </div>
     <img src={getImageUrl("hero/heroImage.png")} alt="hero image of me" className={styles.heroImg}/>
